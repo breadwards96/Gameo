e50f6d0f833fc9cf1f1eaaaba2cfd22527f93ae0
BrandonMac <breadwards96@gmail.com>
2016-11-21 22:29:41 -0800

    Fuck

---
+++ Gameo/src/Start.java
@@ -0,0 +1,48 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Start extends Applet implements Runnable {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		super.start();
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+	
+	@Override
+	public void update(Graphics g) {
+		// TODO Auto-generated method stub
+		super.update(g);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paint(g);
+	}
+	
+}
